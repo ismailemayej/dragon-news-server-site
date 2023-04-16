abf570055d0a4a4269731c25696507bfa3652df6
ismaile hossin <salikbd.com@gamil.com>
2023-04-16 13:13:36 +0600

    first commit

--- index.js
+++ index.js
@@ -5,12 +5,29 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
+const news = require("./Data/news.json");
 const categories = require("./data/categories.json");
 
-app.get("/", (req, res) => {
-  res.send("news api running");
+app.get("/news", (req, res) => {
+  res.send(news);
 });
 
+app.get("/category/:id", (req, res) => {
+  const id = req.params.id;
+  if (id === "08") {
+    res.send(news);
+  } else {
+    const category_news = news.filter((n) => n.category_id === id);
+    res.send(category_news);
+  }
+});
+
+app.get("/news/:id", (req, res) => {
+  const id = req.params.id;
+  console.log(req.params.id);
+  const selectedNews = news.find((n) => n._id === id);
+  res.send(selectedNews);
+});
 app.get("/news-categories", (req, res) => {
   res.send(categories);
 });
